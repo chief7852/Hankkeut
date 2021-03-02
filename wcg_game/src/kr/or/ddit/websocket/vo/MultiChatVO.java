08c2b0c2d73b78fee4b4993e04de6c546448c642
ISSAC7852 <su951100@gmail.com>
2021-03-02 11:00:26 +0900

    채팅 다시 수정

--- wcg_game/src/kr/or/ddit/websocket/vo/MultiChatVO.java
+++ wcg_game/src/kr/or/ddit/websocket/vo/MultiChatVO.java
@@ -5,6 +5,9 @@ import javax.websocket.Session;
 public class MultiChatVO {
 	private String name;
 	private Session session;
+	private String base_img;
+	
+	
 	
 	public MultiChatVO(String name, Session session) {
 		super();
@@ -27,6 +30,16 @@ public class MultiChatVO {
 	public void setSession(Session session) {
 		this.session = session;
 	}
+
+	public String getBase_img() {
+		return base_img;
+	}
+
+	public void setBase_img(String base_img) {
+		this.base_img = base_img;
+	}
+
+
 	
 	
 }
